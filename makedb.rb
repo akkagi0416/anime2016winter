98b443694ba8b280b948a2abdfa96bd56ae0d7e4
akkagi0416 <akkagi0416@gmail.com>
2016-01-28 17:04:38 +0900

    make db from title.md

---
+++ makedb.rb
@@ -0,0 +1,41 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  "adapter" => "sqlite3",
+  "database" => "./anime.db"
+)
+
+class Anime < ActiveRecord::Base
+end
+
+doc = open('title.md').read
+
+doc.each_line do |line|
+  line.match(/「(.+?)」.*\((.*)\)/)
+  title = $1
+  url   = $2
+
+  if url.match(%r{twitter.com/(.+)})
+    screen_name = $1.gsub(/\?lang=ja/, '') 
+  else
+    official = url
+  end
+
+  anime = Anime.find_or_create_by(title: title)
+
+  if official != nil
+    anime.update(official: official)
+  end
+
+  if screen_name != nil
+    anime.update(screen_name: screen_name)
+  end
+
+  # if official != nil
+  #   puts "#{title} official #{official}"
+  # end
+  #
+  # if screen_name != nil
+  #   puts "#{title} @twitter #{screen_name}"
+  # end
+end
